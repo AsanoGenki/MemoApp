4fd05a2573aa3620e5266b4b523105fbb864fb9c
AsanoGenki <asanogenki0218@icloud.com>
2023-09-22 15:22:26 +0900

    First commit.

---
+++ MemoApp/MemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppApp.swift
+//  MemoApp
+//
+//  Created by Genki on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
